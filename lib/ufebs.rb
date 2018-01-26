fce7e15b84c8cf0c7498b128579e19f610e068f0
Evgeniy Burdaev <hate2me@yandex.ru>
2018-01-26 16:58:38 +0300

    fix doc examples

--- lib/ufebs.rb
+++ lib/ufebs.rb
@@ -16,7 +16,7 @@ module Ufebs
   # Получение Сессии(Номера рейса)
   # @return [Ufebs::Entities::Session]
   # @example Пример
-  # session = Ufebs::Session(File.read('doc.xml')) #=> #<Ufebs::Entities::Session:0x00..>
+  #   session = Ufebs::Session(File.read('doc.xml')) #=> #<Ufebs::Entities::Session:0x00..>
   def Session(params)
     params.is_a?(Hash) ? Ufebs::Entities::Session.new(params) : Ufebs::Entities::Session.parse(params, single: true)
   end
@@ -52,22 +52,22 @@ module Ufebs
 
   # Создание/Парсинг выписки ED211
   # @example Пример
-  # ed211 = Ufebs::Requests::Receipt.new(
-  #   number: '8',
-  #   ed_date: '2003-04-14',
-  #   ed_author: '4525545000',
-  #   ed_receiver: '4525000000',
-  #   abstract_date: Date.today,
-  #   abstract_kind: '1',
-  #   acc: '30101810945250000420',
-  #   bic: '044525000',
-  #   end_time: '00:47:07',
-  #   enter_bal: '72619100',
-  #   inquiry_session: '0',
-  #   last_movet_date: Date.today,
-  #   out_bal: '72619100',
-  #   rtgs_unconfirmed_ed: '0'
-  # )
+  #   ed211 = Ufebs::Requests::Receipt.new(
+  #     number: '8',
+  #     ed_date: '2003-04-14',
+  #     ed_author: '4525545000',
+  #     ed_receiver: '4525000000',
+  #     abstract_date: Date.today,
+  #     abstract_kind: '1',
+  #     acc: '30101810945250000420',
+  #     bic: '044525000',
+  #     end_time: '00:47:07',
+  #     enter_bal: '72619100',
+  #     inquiry_session: '0',
+  #     last_movet_date: Date.today,
+  #     out_bal: '72619100',
+  #     rtgs_unconfirmed_ed: '0'
+  #   )
   def ED211(params)
     params.is_a?(Hash) ? Ufebs::Requests::Receipt.new(params) : Ufebs::Requests::Receipt.parse(params)
   end
