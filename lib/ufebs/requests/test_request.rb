5bfd3b322de7ebc58f00f9c95f70a3b6f044a250
Evgeniy Burdaev <hate2me@yandex.ru>
2018-06-30 16:15:58 +0300

    ED999 update and new schemas

--- lib/ufebs/requests/test_request.rb
+++ lib/ufebs/requests/test_request.rb
@@ -11,9 +11,11 @@ module Ufebs
       attribute :number, String, tag: 'EDNo'
       attribute :ed_date, String, tag: 'EDDate'
       attribute :ed_author, String, tag: 'EDAuthor'
+      attribute :creation_date_time, String, tag: 'CreationDateTime'
 
       def initialize(params={})
         @number, @ed_date, @ed_author = params[:number], params[:ed_date], params[:ed_author]
+        @creation_date_time = DateTime.parse(params[:creation_date_time].to_s).strftime("%Y-%m-%dT%H:%M:%SZ")
         super()
       end
 
