4d7670feb272b3b0eeb6ab2b1b4fe662c76ab60e
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-09-29 20:20:21 +0300

    cleanup

--- lib/ufebs/documents/payer_order.rb
+++ lib/ufebs/documents/payer_order.rb
@@ -10,25 +10,26 @@ module Ufebs
       tag 'ED105'
       namespace 'ed'
 
-      attribute :number,             String,                               tag: 'EDNo'
-      attribute :ed_date,            String,                               tag: 'EDDate'
-      attribute :ed_author,          String,                               tag: 'EDAuthor'
-      attribute :sum,                String,                               tag: 'Sum'
-      attribute :payt_kind,          String,                               tag: 'PaytKind'
-      attribute :trans_kind,         String,                               tag: 'TransKind'
-      attribute :uin,                String,                               tag: 'PaymentID'
-      attribute :system_code,        String,                               tag: 'SystemCode'
-      attribute :priority,           String,                               tag: 'Priority'
-      attribute :payment_precedence, String,                               tag: 'PaymentPrecedence'
-      has_one   :acc_doc,            ::Ufebs::Entities::AccDoc,            tag: 'AccDoc'
-      has_one   :payer,              ::Ufebs::Entities::Participant,       tag: 'Payer'
-      has_one   :payee,              ::Ufebs::Entities::Participant,       tag: 'Payee'
-      element   :purpose,            String,                               tag: 'Purpose'
-      has_one   :processing_details, ::Ufebs::Entities::ProcessingDetails, tag: 'ProcessingDetails', state_when_nil: false
-      has_one   :departmental_info,  ::Ufebs::Entities::DepartmentalInfo,  tag: 'DepartmentalInfo', state_when_nil: false
+      attribute :number,             String, tag: 'EDNo'
+      attribute :ed_date,            String, tag: 'EDDate'
+      attribute :ed_author,          String, tag: 'EDAuthor'
+      attribute :sum,                String, tag: 'Sum'
+      attribute :payt_kind,          String, tag: 'PaytKind'
+      attribute :trans_kind,         String, tag: 'TransKind'
+      attribute :uin,                String, tag: 'PaymentID'
+      attribute :system_code,        String, tag: 'SystemCode'
+      attribute :priority,           String, tag: 'Priority'
+      attribute :payment_precedence, String, tag: 'PaymentPrecedence'
+      attribute :trans_content,      String, tag: 'TransContent'
 
-      attribute :trans_content, String,                         tag: 'TransContent'
-      has_one   :partial_payt,  ::Ufebs::Entities::PartialPayt, tag: 'PartialPayt'
+      element :purpose, String, tag: 'Purpose'
+
+      has_one :acc_doc,            ::Ufebs::Entities::AccDoc,            tag: 'AccDoc'
+      has_one :payer,              ::Ufebs::Entities::Participant,       tag: 'Payer'
+      has_one :payee,              ::Ufebs::Entities::Participant,       tag: 'Payee'
+      has_one :partial_payt,       ::Ufebs::Entities::PartialPayt,       tag: 'PartialPayt'
+      has_one :processing_details, ::Ufebs::Entities::ProcessingDetails, tag: 'ProcessingDetails', state_when_nil: false
+      has_one :departmental_info,  ::Ufebs::Entities::DepartmentalInfo,  tag: 'DepartmentalInfo',  state_when_nil: false
     end
   end
 end
--- lib/ufebs/fields/payment_doc.rb
+++ lib/ufebs/fields/payment_doc.rb
@@ -2,25 +2,26 @@ module Ufebs
   module Fields
     module PaymentDoc
       def self.included(content)
-        content.attribute :number, String, tag: 'EDNo'
-        content.attribute :ed_date, String, tag: 'EDDate'
-        content.attribute :ed_author, String, tag: 'EDAuthor'
-        content.attribute :sum, String, tag: 'Sum'
-        content.attribute :payt_kind, String, tag: 'PaytKind'
-        content.attribute :trans_kind, String, tag: 'TransKind'
-        content.attribute :uin, String, tag: 'PaymentID'
-        content.attribute :charge_off_date, String, tag: 'ChargeOffDate'
-        content.attribute :receipt_date, String, tag: 'ReceiptDate'
-        content.attribute :system_code, String, tag: 'SystemCode'
-        content.attribute :priority, String, tag: 'Priority'
+        content.attribute :number,             String, tag: 'EDNo'
+        content.attribute :ed_date,            String, tag: 'EDDate'
+        content.attribute :ed_author,          String, tag: 'EDAuthor'
+        content.attribute :sum,                String, tag: 'Sum'
+        content.attribute :payt_kind,          String, tag: 'PaytKind'
+        content.attribute :trans_kind,         String, tag: 'TransKind'
+        content.attribute :uin,                String, tag: 'PaymentID'
+        content.attribute :charge_off_date,    String, tag: 'ChargeOffDate'
+        content.attribute :receipt_date,       String, tag: 'ReceiptDate'
+        content.attribute :system_code,        String, tag: 'SystemCode'
+        content.attribute :priority,           String, tag: 'Priority'
         content.attribute :payment_precedence, String, tag: 'PaymentPrecedence'
-        content.has_one :acc_doc, ::Ufebs::Entities::AccDoc, tag: 'AccDoc'
-        content.has_one :payer, ::Ufebs::Entities::Participant, tag: 'Payer'
-        content.has_one :payee, ::Ufebs::Entities::Participant, tag: 'Payee'
+
         content.element :purpose, String, tag: 'Purpose'
 
+        content.has_one :acc_doc,            ::Ufebs::Entities::AccDoc,            tag: 'AccDoc'
+        content.has_one :payer,              ::Ufebs::Entities::Participant,       tag: 'Payer'
+        content.has_one :payee,              ::Ufebs::Entities::Participant,       tag: 'Payee'
+        content.has_one :departmental_info,  ::Ufebs::Entities::DepartmentalInfo,  tag: 'DepartmentalInfo',  state_when_nil: false
         content.has_one :processing_details, ::Ufebs::Entities::ProcessingDetails, tag: 'ProcessingDetails', state_when_nil: false
-        content.has_one :departmental_info, Ufebs::Entities::DepartmentalInfo, tag: 'DepartmentalInfo', state_when_nil: false
       end
     end
   end
