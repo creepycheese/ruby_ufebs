aaa4f4628e16dc65109c2589ee7cac5054384eda
Aleksandr Klimov <efelikanfuzin@gmail.com>
2019-09-13 11:37:12 +0300

    Правильно подключил common модуль (#29)
    * Правильно подключил common модуль
    
    * Update .gitignore

--- lib/ufebs/entities/request_info.rb
+++ lib/ufebs/entities/request_info.rb
@@ -1,23 +1,25 @@
 # frozen_string_literal: true
 
+require_relative '../../ufebs/modules/common'
+
 module Ufebs
   module Entities
     class DateTimeInterval
       include HappyMapper
-      # include Ufebs::Common
+      include Ufebs::Common
 
       attribute :begin_time, String, tag: 'BeginTime'
       attribute :end_time, String, tag: 'EndTime'
 
       def initialize(begin_time: '', end_time: '')
-        @begin_time = DateTime.parse(begin_time.to_s).strftime('%Y-%m-%dT%H:%M:%SZ')# if present?(begin_time)
-        @end_time = DateTime.parse(end_time.to_s).strftime('%Y-%m-%dT%H:%M:%SZ')# if present?(end_time)
+        @begin_time = DateTime.parse(begin_time.to_s).strftime('%Y-%m-%dT%H:%M:%SZ') if present?(begin_time)
+        @end_time = DateTime.parse(end_time.to_s).strftime('%Y-%m-%dT%H:%M:%SZ') if present?(end_time)
       end
     end
 
     class BusinessDay
       include HappyMapper
-      # include Ufebs::Common
+      include Ufebs::Common
 
       attribute :abstract_date, String, tag: 'AbstractDate'
 
@@ -28,7 +30,7 @@ module Ufebs
 
     class RequestInfo
       include HappyMapper
-      # include Ufebs::Common
+      include Ufebs::Common
 
       register_namespace 'ed', 'urn:cbr-ru:ed:v2.0'
       namespace 'ed'
@@ -45,11 +47,11 @@ module Ufebs
         params.each do |key, value|
           case key.to_sym
           when :date_time_interval
-            # next unless present?(value[:begin_time]) && present?(value[:end_time])
+            next unless present?(value[:begin_time]) && present?(value[:end_time])
 
             @date_time_interval = DateTimeInterval.new(begin_time: value[:begin_time], end_time: value[:end_time])
           when :business_day
-            # next unless present?(value[:abstract_date])
+            next unless present?(value[:abstract_date])
 
             @business_day = BusinessDay.new(abstract_date: value[:abstract_date])
           else instance_variable_set("@#{key}".to_sym, value)
--- lib/ufebs/requests/account_statement.rb
+++ lib/ufebs/requests/account_statement.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-# require_relative '../../ufebs/modules/common'
+require_relative '../../ufebs/modules/common'
 
 module Ufebs
   module Requests
     class AccountStatement
       include HappyMapper
-      # include Ufebs::Common
+      include Ufebs::Common
 
       register_namespace 'ed', 'urn:cbr-ru:ed:v2.0'
 
@@ -30,9 +30,9 @@ module Ufebs
           when :ed_date then @ed_date = Date.parse(value.to_s).strftime('%Y-%m-%d')
           when :abstract_date then @abstract_date = Date.parse(value.to_s).strftime('%Y-%m-%d')
           when :begin_time
-            @begin_time = Time.parse(value.to_s).strftime('%H:%M:%S')# if present?(value)
+            @begin_time = Time.parse(value.to_s).strftime('%H:%M:%S') if present?(value)
           when :end_time
-            @end_time = Time.parse(value.to_s).strftime('%H:%M:%S')# if present?(value)
+            @end_time = Time.parse(value.to_s).strftime('%H:%M:%S') if present?(value)
           else instance_variable_set("@#{key}".to_sym, value)
           end
         end
