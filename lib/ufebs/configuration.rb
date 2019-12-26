618b2c71385d8d3acb6607bca2e403352943224e
Aleksandr Klimov <al.klimov@rocketguys.com>
2019-12-26 18:36:57 +0300

    Add new XML Schemas 2020

--- lib/ufebs/configuration.rb
+++ lib/ufebs/configuration.rb
@@ -10,7 +10,8 @@ module Ufebs
       {
         '2017' => File.expand_path('../../../XMLSchemas/cbr_ed_kbr_v2017.4.2.xsd', __FILE__),
         '2018' => File.expand_path('../../../XMLSchemas_2018/cbr_ed_kbr_v2018.3.2.xsd', __FILE__),
-        '2019' => File.expand_path('../../../XMLSchemas_2019/cbr_ed_kbr_v2019.2.1.xsd', __FILE__)
+        '2019' => File.expand_path('../../../XMLSchemas_2019/cbr_ed_kbr_v2019.2.1.xsd', __FILE__),
+        '2020' => File.expand_path('../../../XMLSchemas_2020/cbr_ed_kbr_v2020.1.1.xsd', __FILE__)
       }
     end
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ufebs'
 
 Ufebs.configure do |config|
-  config.standard = '2019'
+  config.standard = '2020'
 end
 
 XML_VALIDATION = Ufebs.validation_schema
