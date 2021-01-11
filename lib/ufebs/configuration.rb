9ccfbe7a03991b12d54510711e6e2c24c4ef6321
Evgeny Burdaev <hate2me@yandex.ru>
2021-01-11 16:23:21 +0300

    XML Schemas

--- lib/ufebs/configuration.rb
+++ lib/ufebs/configuration.rb
@@ -11,7 +11,8 @@ module Ufebs
         '2017' => File.expand_path('../../../XMLSchemas/cbr_ed_kbr_v2017.4.2.xsd', __FILE__),
         '2018' => File.expand_path('../../../XMLSchemas_2018/cbr_ed_kbr_v2018.3.2.xsd', __FILE__),
         '2019' => File.expand_path('../../../XMLSchemas_2019/cbr_ed_kbr_v2019.2.1.xsd', __FILE__),
-        '2020' => File.expand_path('../../../XMLSchemas_2020/cbr_ed_kbr_v2020.3.0.xsd', __FILE__)
+        '2020' => File.expand_path('../../../XMLSchemas_2020/cbr_ed_kbr_v2020.3.0.xsd', __FILE__),
+        '2021' => File.expand_path('../../../XMLSchemas_2021/cbr_ed_kbr_v2021.1.0.xsd', __FILE__)
       }
     end
   end
